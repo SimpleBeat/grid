4190e5f4a64c53230811e147c53679c7d8941df3
Ilya Katulin <simplebeat.katulin@gmail.com>
2017-02-16 16:17:09 +0300

    Updated javascript and style

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // Grid JavaScript (requires jQuery)
+
+$(document).ready(function(){
+  var gridWidth=$(".grid").width();
+  var cellsInRow=64;
+
+  makeAGrid(cellsInRow);
+
+});
+
+function makeAGrid(cells) {
+  var dta1='<div class="cellRow"></div>';
+  var dta2='<div class="cell"></div>';
+
+  for (var i=0; i<cells; i++) {
+    $('.grid').append(dta1);
+  }
+  for (var j=0; j<cells; j++) {
+    $('.grid').find('.cellRow').append(dta2);
+  }
+
+  switch (cells) {
+    case 64: {
+      $('.cell').css({'width': '9px', 'height': '9px'});
+      $('.cellRow').css({'height': '9px'});
+      break;
+    }
+    case 32: {
+      $('.cell').css("width", "20px");
+      $('.cell').css("height", "20px");
+      $('.cellRow').css("height", "20px");
+      break;
+    }
+    default: {
+      $('.cell').css({'width': '40px', 'height': '40px'});
+    }
+  }
+
+
+};
